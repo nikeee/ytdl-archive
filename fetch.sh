699c4f66e6ac474566128fc938fdc0e476965c36
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2023-02-02 21:06:12 +0100

    Add last-sync

--- fetch.sh
+++ fetch.sh
@@ -6,4 +6,9 @@ YTDL="yt-dlp"
 
 YTDL_OPTS="-4 -iwc -R 100 --no-post-overwrites"
 
-$YTDL $YTDL_OPTS --download-archive /data/archive.txt --batch-file /data/sources.txt -o "/data/files/(title)s-%(id)s.%(ext)s"
+$YTDL $YTDL_OPTS \
+  --download-archive /data/archive.txt \
+  --batch-file /data/sources.txt \
+  -o "/data/files/(title)s-%(id)s.%(ext)s"
+
+touch /data/last-sync.txt
