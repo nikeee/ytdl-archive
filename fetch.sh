6118dc155380bb92d32ec0d96e64fb045b1689da
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2024-07-07 20:56:19 +0200

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -9,6 +9,6 @@ YTDL_OPTS="-4 -iwc -R 100 --no-post-overwrites"
 $YTDL $YTDL_OPTS \
   --download-archive /data/archive.txt \
   --batch-file /data/sources.txt \
-  -o "/data/files/%(extractor)s/%(uploader)s/%(upload_date)s-%(title)s-%(duration)ss-%(id)s.%(ext)s"
+  -o "/data/files/%(extractor)s/%(uploader_id)s/%(upload_date)s-%(title)s-%(duration)ss-%(id)s.%(ext)s"
 
 touch /data/last-sync.txt
