e034425d38db9dd6afa2760b24073e6a8121478e
Niklas Mollenhauer <nikeee@users.noreply.github.com>
2026-04-11 04:18:09 +0200

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,11 +4,12 @@ set -eux
 
 # YTDL="youtube-dl"
 YTDL="yt-dlp"
-YTDL_OPTS="-4 -iwc -R 100 --no-post-overwrites"
+YTDL_OPTS="-4 -iwc -R 100 --no-post-overwrites --restrict-filenames"
 
 $YTDL $YTDL_OPTS \
   --download-archive /data/archive.txt \
   --batch-file /data/sources.txt \
+  --write-info-json --write-thumbnail \
   -o "/data/files/%(extractor)s/%(uploader_id)s/%(upload_date)s-%(title)s-%(duration)s-%(id)s.%(ext)s"
 
 touch /data/last-sync.txt
